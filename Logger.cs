9ab20ec55b6269a38c7807e16324025ad8290ffc
Gabriel Cardinal <gabrielc@outlook.fr>
2019-01-16 18:49:07 -0500

    Used the System.ConsoleColor enum to implement different color logging

--- Logger.cs
+++ Logger.cs
@@ -2,21 +2,65 @@ using System;
 
 namespace NarcityMedia.Log
 {
+    /// <summary>
+    /// Represents a log level.
+    /// The value of those colors correspond to the values of the <see cref="System.ConsoleColor"/> Enum
+    /// see https://docs.microsoft.com/en-us/dotnet/api/system.consolecolor?view=netframework-4.7.2
+    /// </summary>
+    public enum LogType
+    {
+        Success = 10,
+        Warning = 14,
+        Error = 12,
+        Info = 11
+    }
+
+    /// <summary>
+    /// Utility that allows fancy logging
+    /// </summary>
     static public class Logger
     {
-        public enum LogType
+        /// <summary>
+        /// Gets or sets the character that will separate the prefix and the logged message
+        /// </summary>
+        public static char Separator = '|';
+
+        /// <summary>
+        /// Logs a given message to the console without changing the current console foreground color.
+        /// Additionally, prefixes the log with DateTime.Now
+        /// </summary>
+        /// <param name="message">The message to log to the console</param>
+        public static void Log(string message)
         {
-            Success, Warning, Error, Info
+            WritePrefix();
+            Console.WriteLine(message);
         }
 
-        // ANSI color tokens must match the index of the LogType enum
-        private static readonly string[] ANSITokens = { "\u001b[32m", "\u001b[33m", "\u001b[31m", "\u001b[34m" };
-
-        private static readonly string ANSIReset = "\u001b[0m";
-
+        /// <summary>
+        /// Logs a given message to the console with a color that corresponds to the given
+        /// <see cref="Logger.LogType" />.
+        /// Additionally, prefixes the log with DateTime.Now
+        /// </summary>
+        /// <param name="message">The message to log to the console</param>
+        /// <param name="logType">The desired LoType.static This determines the log color</param>
         public static void Log(string message, LogType logType)
         {
-            Console.WriteLine(String.Format("{0} | {1}{2}{3}", DateTime.Now.ToString("dd/mm/yyyy - HH:mm:ss"), ANSITokens[(int)logType], message, ANSIReset));
+            ConsoleColor oldColor = Console.ForegroundColor;
+
+            WritePrefix();
+
+            Console.ForegroundColor = (ConsoleColor) ((int) logType);
+            Console.WriteLine(message);
+
+            Console.ForegroundColor = oldColor;
+        }
+
+        /// <summary>
+        /// Writes the prefix and the separator without inserting a new line
+        /// </summary>
+        private static void WritePrefix()
+        {
+            Console.Write(String.Format("{0} {1} ", DateTime.Now.ToString("dd/mm/yyyy - HH:mm:ss"), Logger.Separator));
         }
     }
 }
